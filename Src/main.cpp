8e24c6a5248c13dac5a408c4274d5a3b7f4d6e89
unknown <s.elouarrri4684@uca.ac.ma>
2026-04-13 15:23:06 +0100

    test2

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "../include/player.hpp"
 
@@ -7,14 +6,4 @@ int main() {
     p.move();
     std::cout << "Project started" << std::endl;
     return 0;
-=======
-#include <iostream>
-#include "../include/player.hpp"
-
-int main() {
-    Player p;
-    p.move();
-    std::cout << "Project started" << std::endl;
-    return 0;
->>>>>>> 031d84e0fc9afef5425224eec2c16725bf25dfc5
 }

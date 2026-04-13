2a84c2b027404348cf2dd0184a0e852cfe242262
unknown <s.elouarrri4684@uca.ac.ma>
2026-04-13 15:59:53 +0100

    l'ajout de game.hpp

---
+++ Src/game.cpp
@@ -0,0 +1,26 @@
+#include "Game.h"
+#include <iostream>
+
+Game::Game() {}
+
+void Game::run() {
+    std::cout << "Game started" << std::endl;
+
+    while (true) {
+        processEvents();
+        update();
+        render();
+    }
+}
+
+void Game::processEvents() {
+    // input later
+}
+
+void Game::update() {
+    // game logic later
+}
+
+void Game::render() {
+    // display later
+}

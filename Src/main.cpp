c57ccb730629645f4098e68c7ba8730251678bca
unknown <s.elouarrri4684@uca.ac.ma>
2026-04-17 11:32:19 +0100

    La suppression du contenu test

--- Src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../include/player.hpp"
-
-int main() {
-    Player p;
-    p.update();//Commentaire fghgdsfjfdjfrjhrejhgrjrejkrejkhrejkhrgjh
-    std::cout << "Project started" << std::endl;
-    return 0;
-}
